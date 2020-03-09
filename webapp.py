c26a55579fa2e63c529da4bd83bc53c19100de2c
LEGOSROCKDUDE86 <arriagabrandon231@gmail.com>
2020-03-09 09:34:43 -0700

    made variables global

--- webapp.py
+++ webapp.py
@@ -2,8 +2,10 @@ from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
 from flask import render_template
 
-import pprint
+import pymongo
 import os
+import sys
+import pprint
 
 # This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
 # Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
@@ -14,19 +16,25 @@ app = Flask(__name__)
 
 app.debug = True #Change this to False for production
 
-app.secret_key = os.environ['SECRET_KEY'] 
+app.secret_key = os.environ['SECRET_KEY']
 oauth = OAuth(app)
+
+connection_string = os.environ["MONGO_CONNECTION_STRING"]
+db_name = os.environ["MONGO_DBNAME"]
+client = pymongo.MongoClient(connection_string)
+db = client[db_name]
+collection = db['Data']
 
 #Set up Github as OAuth provider
 github = oauth.remote_app(
     'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], 
+    consumer_key=os.environ['GITHUB_CLIENT_ID'],
     consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
     request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',  
+    access_token_url='https://github.com/login/oauth/access_token',
     authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
@@ -40,7 +48,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/login')
-def login():   
+def login():
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
@@ -53,12 +61,12 @@ def authorized():
     resp = github.authorized_response()
     if resp is None:
         session.clear()
-        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)
     else:
         try:
             #save user data and set log in message
             session["github_token"] = (resp['access_token'], '')
-            session['user_data'] = github.get('user').data 
+            session['user_data'] = github.get('user').data
             if session['user_data']['public_repos'] > 9:
                 message = 'You were successfully logged in as ' + session['user_data']['login']
             else:
@@ -74,11 +82,8 @@ def authorized():
 
 @app.route('/page1')
 def renderPage1():
-    if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
-    else:
-        user_data_pprint = '';
-    return render_template('page1.html',dump_user_data=user_data_pprint)
+        test = collection.find_one()
+    return render_template('page1.html', testdata = test)
 
 @app.route('/page2')
 def renderPage2():
@@ -91,6 +96,7 @@ def renderPage2():
 @github.tokengetter
 def get_github_oauth_token():
     return session['github_token']
+
 
 
 if __name__ == '__main__':

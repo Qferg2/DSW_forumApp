5bac24632b63932214362b77e6dfff1098eb04b9
Brandon <53132252+LEGOSROCKDUDE86@users.noreply.github.com>
2020-04-21 09:49:52 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -94,7 +94,7 @@ def renderPage1():
             print(post_id)
         posts = db.Data
         for document in posts.find():
-            options += document['User'] + '\t' + document['Message'] + '\n' + Markup('<form action="/page1" method="post"> <button type="submit" name="delete" value="fname">Delete</button> </form>')
+            options += document['User'] + '\t' + document['Message'] + '\n' + Markup('<form action="/page1" method="post"> <button type="submit" name="delete" value="5e8f558d9f944ce49c549952">Delete</button> </form>')
         if 'delete' in request.form: 
             id = ObjectId(request.form['delete'])
             db.posts.delete_one({'_id':id})
